f5bcfeaacf6535e96fd50abf0d6396313d54fc3c
Markab-1 <velichko.anna.b@gmail.com>
2022-01-03 23:28:40 +0200

    2

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,14 @@
 const inputText = document.querySelector('input#validation-input');
 
+
+
 inputText.addEventListener("blur", () => {
     if (inputText.value.length < inputText.dataset.length) {
+        inputText.classList.remove('valid');
         inputText.classList.add('invalid');
     }
-    else {inputText.classList.add('valid');};
+    else {
+        inputText.classList.remove('invalid');
+        inputText.classList.add('valid');
+    };
 });
